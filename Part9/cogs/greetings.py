dc5465fcd55a822222c0ae480bd27d02b595d728
Amit thapliyal <thapliyalamit2001@gmail.com>
2022-10-10 19:13:49 +0530

    second

--- Part9/cogs/greetings.py
+++ Part9/cogs/greetings.py
@@ -4,14 +4,15 @@ from discord.ext import commands
 
 
 class Greetings(commands.Cog):
-    def __init__(self,client):
+    def __init__(self, client):
         self.client = client
 
-
-        
     @commands.command()
-    async def hello(self,ctx):
-        await ctx.send(f'Hello {ctx.author} from Anonymous Bot!!')
+    async def hello(self, ctx):
+
+        embed = discord.Embed(title='My EMbed', description='Cool')
+        await ctx.send(embed=embed)
+
 
 async def setup(client):
     await client.add_cog(Greetings(client))
